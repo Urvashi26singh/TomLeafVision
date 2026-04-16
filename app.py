7bb1503b0f130851227a9babbeb85e4c806e1874
Urvashi26singh <urvashi245tanu@gmail.com>
2026-04-16 06:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -538,4 +538,4 @@ if __name__ == "__main__":
     print("="*60 + "\n")
 
     # Run the app
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)  # try 5000 or 8080
